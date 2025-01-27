50a4306aef3db7c66fbde2f976b26cddccc636f6
ativerc <Ativerc@users.noreply.github.com>
2025-01-27 19:59:45 +0530

    feat: Added max-time to the curl commands. and Default values to MAINNET and SECONDNET

--- openwrt/isp-names.sh
+++ openwrt/isp-names.sh
@@ -3,8 +3,11 @@
 MAIN_IFACE=pppoe-wan
 SECOND_IFACE=lan1
 
-MAINNET=$(curl -s --interface $MAIN_IFACE "http://ip-api.com/json?fields=isp" | jq .isp)
-SECONDNET=$(curl -s --interface $SECOND_IFACE "http://ip-api.com/json?fields=isp" | jq .isp)
+MAINNET=$(curl -s --interface $MAIN_IFACE --max-time 2 "http://ip-api.com/json?fields=isp" | jq .isp)
+SECONDNET=$(curl -s --interface $SECOND_IFACE --max-time 2 "http://ip-api.com/json?fields=isp" | jq .isp)
+
+MAINNET="${MAINNET:-MainNet}"
+SECONDNET="${SECONDNET:-AltNet}"
 
 echo $MAINNET
 echo $SECONDNET
